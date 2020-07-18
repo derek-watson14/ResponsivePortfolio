ccbe57feedd6ab660bb328b22a818bc4f0dd985f
Derek Watson <derek.watson92@gmail.com>
2020-07-17 22:17:35 -0700

    created languages pie chart

--- assets/index.js
+++ assets/index.js
@@ -50,8 +50,10 @@ async function getRepoNames() {
     url: `https://api.github.com/users/derek-watson14/repos?per_page=100`,
     method: "GET",
   });
-  return userRepos;
+  const repoNames = userRepos.map((repo) => repo.name);
+  return repoNames;
 }
+
 async function getRepoCommits(repo, page = 1) {
   let commits = $.ajax({
     url: `https://api.github.com/repos/derek-watson14/${repo}/commits?per_page=100&author=derek-watson14&page=${page}`,
@@ -59,16 +61,16 @@ async function getRepoCommits(repo, page = 1) {
   });
   return commits;
 }
+
 const getDates = (commits) => {
   return commits.map(({ commit }) => {
     let date = commit.author.date.split("-");
     return `${date[0]}-${date[1]}`;
   });
 };
-async function getAllCommitDates() {
+
+async function getAllCommitDates(repoNames) {
   const commitDates = [];
-  const userRepos = await getRepoNames();
-  const repoNames = userRepos.map((repo) => repo.name);
 
   for (const repo of repoNames) {
     const commits = await getRepoCommits(repo);
@@ -82,9 +84,40 @@ async function getAllCommitDates() {
   return commitDates;
 }
 
-// * Draw Chart
-function drawChart(labels, data) {
-  var ctx = document.getElementById("myChart");
+async function getRawLanguageStats(repoName) {
+  const repoStats = await $.ajax({
+    url: `https://api.github.com/repos/derek-watson14/${repoName}/languages`,
+    method: "GET",
+  });
+  return repoStats;
+}
+
+async function getLanguageStats(repoNames) {
+  const statsByRepo = {};
+  const statsByLanguage = {};
+  for (const repo of repoNames) {
+    const repoStats = await getRawLanguageStats(repo);
+    statsByRepo[repo] = repoStats;
+  }
+  for (const [key, value] of Object.entries(statsByRepo)) {
+    if (!$.isEmptyObject(value)) {
+      for (const [lang, amount] of Object.entries(value)) {
+        if (statsByLanguage[lang]) {
+          statsByLanguage[lang] += amount;
+        } else {
+          statsByLanguage[lang] = amount;
+        }
+      }
+    }
+  }
+  const data = Object.values(statsByLanguage);
+  const labels = Object.keys(statsByLanguage);
+  return { data, labels };
+}
+
+// * Draw Charts
+function drawBarChart(labels, data) {
+  var ctx = document.getElementById("commitsChart");
   var myChart = new Chart(ctx, {
     type: "bar",
     data: {
@@ -102,235 +135,61 @@ function drawChart(labels, data) {
   });
 }
 
-getAllCommitDates().then(function (data) {
-  let tally = data.sort().reduce((tally, date) => {
+function drawPieChart(labels, data) {
+  var ctx = document.getElementById("languagesChart");
+  var myChart = new Chart(ctx, {
+    type: "doughnut",
+    data: {
+      datasets: [
+        {
+          data,
+          backgroundColor: [
+            "#4b6892",
+            "#ca936a",
+            "#92564b",
+            "#844b92",
+            "#93ae5a",
+            "#1b2127",
+          ],
+          label: "Languages",
+        },
+      ],
+      labels,
+    },
+    options: {
+      responsive: true,
+      legend: {
+        labels: {
+          padding: 20,
+        },
+        position: "left",
+      },
+    },
+  });
+}
+
+let stats;
+async function displayGithubData() {
+  const repoNames = await getRepoNames();
+  console.log("Loadin'");
+
+  // TODO display indicator while loading
+
+  const languageStats = await getLanguageStats(repoNames);
+  drawPieChart(languageStats.labels, languageStats.data);
+
+  const commitDates = await getAllCommitDates(repoNames);
+  let tally = commitDates.sort().reduce((tally, date) => {
     date = date.split("-");
     const displayFormat = `${months[date[1] - 1]} '${date[0].slice(2)}`;
     tally[displayFormat] = (tally[displayFormat] || 0) + 1;
     return tally;
   }, createEmptyBins());
 
-  // TODO display indicator while loading
-
   let chartLabels = Object.keys(tally);
   let chartData = Object.values(tally);
 
-  drawChart(chartLabels, chartData);
-});
+  drawBarChart(chartLabels, chartData);
+}
 
-// * Testing with example data:
-let example = [
-  "2020-06-17",
-  "2020-06-17",
-  "2020-06-17",
-  "2020-06-17",
-  "2020-06-17",
-  "2020-06-17",
-  "2020-06-17",
-  "2020-06-17",
-  "2020-06-16",
-  "2020-06-15",
-  "2020-06-11",
-  "2020-06-11",
-  "2020-06-11",
-  "2020-06-29",
-  "2020-06-27",
-  "2020-06-27",
-  "2020-06-27",
-  "2020-06-26",
-  "2020-06-26",
-  "2020-06-26",
-  "2020-06-26",
-  "2020-06-26",
-  "2020-06-26",
-  "2020-06-26",
-  "2020-06-26",
-  "2020-06-26",
-  "2020-06-26",
-  "2020-06-25",
-  "2020-06-25",
-  "2020-06-25",
-  "2020-06-25",
-  "2020-06-25",
-  "2020-06-25",
-  "2020-06-25",
-  "2020-06-25",
-  "2020-06-25",
-  "2020-06-25",
-  "2020-06-25",
-  "2020-06-25",
-  "2020-06-25",
-  "2020-06-24",
-  "2020-06-24",
-  "2020-06-24",
-  "2020-06-24",
-  "2020-06-24",
-  "2020-06-23",
-  "2020-06-23",
-  "2020-06-22",
-  "2020-06-22",
-  "2020-06-11",
-  "2019-07-22",
-  "2019-07-19",
-  "2019-07-19",
-  "2019-07-19",
-  "2019-07-18",
-  "2019-07-18",
-  "2019-07-18",
-  "2019-07-18",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-16",
-  "2019-07-03",
-  "2019-07-03",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-28",
-  "2019-06-27",
-  "2019-06-20",
-  "2019-06-20",
-  "2019-06-20",
-  "2019-06-20",
-  "2019-06-20",
-  "2019-06-20",
-  "2019-06-20",
-  "2019-06-20",
-  "2019-06-20",
-  "2019-06-20",
-  "2019-06-20",
-  "2019-06-20",
-  "2019-06-17",
-  "2019-06-15",
-  "2019-06-15",
-  "2019-06-15",
-  "2019-06-15",
-  "2019-06-15",
-  "2019-06-15",
-  "2019-06-12",
-  "2019-06-12",
-  "2019-06-12",
-  "2019-06-11",
-  "2019-06-11",
-  "2019-06-11",
-  "2019-06-11",
-  "2019-06-11",
-  "2019-06-11",
-  "2019-06-11",
-  "2019-06-11",
-  "2019-06-02",
-  "2019-06-02",
-  "2019-06-02",
-  "2019-06-02",
-  "2019-06-01",
-  "2019-06-01",
-  "2019-06-01",
-  "2019-06-01",
-  "2019-06-01",
-  "2019-06-01",
-  "2019-06-01",
-  "2019-05-28",
-  "2019-03-04",
-  "2020-06-11",
-  "2019-03-04",
-  "2019-03-04",
-  "2020-06-08",
-  "2020-06-03",
-  "2020-06-03",
-  "2020-06-03",
-  "2020-06-11",
-  "2019-04-14",
-  "2019-11-27",
-  "2019-11-27",
-  "2019-11-27",
-  "2019-11-27",
-  "2019-11-27",
-  "2019-11-27",
-  "2019-11-27",
-  "2019-11-27",
-  "2019-11-12",
-  "2020-06-13",
-  "2020-06-12",
-  "2020-06-11",
-  "2020-06-11",
-  "2020-06-11",
-  "2020-06-11",
-  "2020-06-11",
-  "2020-05-31",
-  "2020-05-31",
-  "2020-05-31",
-  "2020-05-31",
-  "2019-12-15",
-  "2019-12-15",
-  "2019-12-12",
-  "2019-12-12",
-  "2019-12-12",
-  "2019-12-12",
-  "2019-12-12",
-  "2019-12-12",
-  "2019-12-12",
-  "2019-12-12",
-  "2019-12-12",
-  "2020-06-11",
-  "2019-04-18",
-  "2019-04-17",
-  "2019-04-15",
-  "2019-04-14",
-  "2019-04-12",
-  "2019-04-12",
-  "2019-04-10",
-  "2019-04-09",
-  "2019-04-08",
-  "2019-04-07",
-];
-
-// let tally = example.sort().reduce((tally, date) => {
-// date = date.split("-");
-// const displayFormat = `${months[date[1] - 1]} '${date[0].slice(2)}`;
-// tally[displayFormat] = (tally[displayFormat] || 0) + 1;
-//   return tally;
-// }, createEmptyBins());
-
-// let chartLabels = Object.keys(tally);
-// let chartValues = Object.values(tally);
-
-// drawChart(chartLabels, chartValues);
+displayGithubData();
